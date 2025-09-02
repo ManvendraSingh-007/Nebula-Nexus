2523771e000f3e0f439ff44b758374354108ac31
ManvendraSingh-007 <montchouhan@gmail.com>
2025-09-02 10:57:09 +0530

    Add database connection setup using SQLAlchemy

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
+
+DATABASE_URL = "mysql+pymysql://sql12796897:hPwq3iZJmL@sql12.freesqldatabase.com:3306/sql12796897" 
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
