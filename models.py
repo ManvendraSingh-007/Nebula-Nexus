4a9bd862b153543609adf8d4b9240c113a1f579e
ManvendraSingh-007 <montchouhan@gmail.com>
2026-01-08 22:57:59 +0530

    Add PendingUser model with fields for username, email, password, OTP code, and verification status

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+import datetime
 
 class User(Base):
     __tablename__ = "users"
@@ -15,3 +16,13 @@ class Message(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     
+
+class PendingUser(Base):
+    __tablename__ = "pending_users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(String(200), nullable=False) # Hashed
+    otp_code = Column(String(20), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    isVerified = Column(Boolean, default=False, nullable=False)
