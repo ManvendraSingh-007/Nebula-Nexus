01bb3ed917e0a27de8a5face722a95ee65b125d8
ManvendraSingh-007 <montchouhan@gmail.com>
2026-01-11 20:56:26 +0530

    refactor: replace direct config variable usage with Config class for consistency in auth.py, Also now we use the user_id to sign and verify jwt instead of user_email and username

--- auth.py
+++ auth.py
@@ -1,12 +1,11 @@
 from datetime import timedelta, datetime, timezone
 from jose import jwt, JWTError
 from fastapi.security import OAuth2PasswordBearer
-from pydantic import EmailStr
 from fastapi import HTTPException, Depends
 from typing import Optional
 from database import Session, get_database
 import models
-from config import SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM
+from config import Config
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -16,35 +15,34 @@ def create_access_token(data: dict, expire_delta:  Optional[int] = None):
     if expire_delta:
         expire = datetime.now(timezone.utc) + timedelta(minutes=expire_delta)
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=Config.ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(
         claims=to_encode,
-        key=SECRET_KEY,
-        algorithm=ALGORITHM
+        key=Config.SECRET_KEY,
+        algorithm=Config.ALGORITHM
     )
 
     return encoded_jwt
 
 def verify_access_token(token: str):
     try:
-        payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=ALGORITHM)
-        # {'sub': 'example@ok.com', 'exp': 176783691}
-        user_email: str = payload.get("sub") 
-        username: str = payload.get("username")
-        if user_email is None:
+        payload = jwt.decode(token=token, key=Config.SECRET_KEY, algorithms=Config.ALGORITHM)
+        # {'sub': '2', 'exp': 176783691}
+        user_id: str = payload.get("sub") 
+        if user_id is None:
             raise HTTPException(status_code=404, detail="Invalid Token")
         
-        return [user_email, username]
+        return user_id
     
     except JWTError as j:
         raise HTTPException(status_code=404, detail=f"{j}")
     
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_database)):
-    user_email = verify_access_token(token=token)
-    user = db.query(models.User).filter(models.User.email == user_email).first()
+    user = verify_access_token(token=token)
+    user = db.query(models.User).filter(models.User.email == user[0]).first()
     if not user:
         raise HTTPException(status_code=401, detail="User not found")
     return user
