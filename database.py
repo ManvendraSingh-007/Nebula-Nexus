e08247140573e9104489ee7c07c89eae48dc1741
ManvendraSingh-007 <montchouhan@gmail.com>
2026-01-11 14:36:01 +0530

    refactor: streamline database credential retrieval by using Config class instead of dotenv

--- database.py
+++ database.py
@@ -1,17 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
-import os
-from dotenv import load_dotenv
+from config import Config
 
-# Load key-value pairs from the .env file into environment variables.
-load_dotenv()
 
 # Pull database credentials from the environment
-database_user = os.getenv("DATABASE_USER")
-database_password = os.getenv("DATABASE_PASSWORD")
-database_host = os.getenv("DATABASE_HOST")
-database_port = os.getenv("DATABASE_PORT")
-database_name = os.getenv("DATABASE_NAME")
+database_user = Config.DB_USER
+database_password = Config.DB_PASSWORD
+database_host = Config.DB_HOST
+database_port = Config.DB_PORT
+database_name = Config.DB_NAME
 
 # Construct the connection string for the MySQL database using the PyMySQL driver
 DATABASE_URL = f"mysql+pymysql://{database_user}:{database_password}@{database_host}:{database_port}/{database_name}"
